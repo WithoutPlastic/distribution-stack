6ef8b15f019754ec75ddbcfc722ae5e18a33a4af
PaprikaZ <sms.extreme@gmail.com>
2015-10-02 18:13:17 +0800

    Add salt stack profile

---
+++ saltstack/script/install.ubuntu.sh
@@ -0,0 +1,9 @@
+echo "INFO Adding saltstack repository ppa"
+add-apt-repository ppa:saltstack/salt
+echo "INFO saltstack repository ppa add done"
+echo "INFO update package indexs"
+apt-get update
+echo "INFO update package done"
+echo "INFO install saltstack packages"
+apt-get install -y salt-master salt-minion salt-syndic salt-cloud salt-ssh salt-api
+echo "INFO install saltstack package done"
