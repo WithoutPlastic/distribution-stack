f893f3f81e0d3560ba07fe52b89ae57d29172648
PaprikaZ <sms.extreme@gmail.com>
2016-02-10 18:15:53 +0800

    Add rabbitmq install script

---
+++ rabbitMQ/install.sh
@@ -0,0 +1,19 @@
+## For Ubuntu 14.04 LTS
+// Install Erlang/OTP 18
+
+- Add `deb http://packages.erlang-solutions.com/ubuntu precise contrib` to apt
+  source list
+- Do `apt-get update`
+- Do `apt-get install erlang`
+
+
+// Install Elixir
+
+- Do `apt-get install elixir`
+
+
+// Install RabbitMQ
+- Do `apt-get install erlang-nox
+- Download RabbitMQ package from http://www.rabbitmq.com
+- Install via dpkg `dpkg -i rabbitmq-server_3.5.4-1_all.deb`
+
