4c963d635e6a8edb23dac66afd94b66bcc01c2ab
WithoutPlastic <without.plastic@outlook.com>
2024-12-20 14:57:33 +0000

    Add new profile powershell

---
+++ profile/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# Load posh-git
+# Import-Module posh-git
+
+# Command Alias
+#New-Alias -Name nvim -Value <\Path\To\Program\Location\Exe>
+
+# Go Programming Language Environment
+#$env:GOPROXY = "https://goproxy.io,direct"
